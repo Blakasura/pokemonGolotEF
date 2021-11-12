428f0f54c89d98741f75787e258881e59c075c82
danip <danip@DESKTOP-SP7DLLE>
2021-11-12 18:34:28 +0100

    Added final encryption function

--- Data/Data.cs
+++ Data/Data.cs
@@ -14,9 +14,14 @@ namespace Data
     public class Database : DbContext
     {
         // Cryptography VAR
-        const string connStringFile = @".\\Connection\\connString.txt";
+        static RSACryptoServiceProvider rsa;
+        static CspParameters cspp = new CspParameters();
+        const string connStringFile = @".\\Connection\\connString.enc";
+        const string keysFile = @".\\Connection\\rsaKeys.txt";
+        const string fileDecrypted = @".\\Connection\decryptedString.txt";
 
         // Import all the tables from Models
+
         public DbSet<Pokemon> Pokemons { get; set; }
         public DbSet<Moviment> Moviments { get; set; }
         public DbSet<Regal> Regals { get; set; }
@@ -209,18 +214,130 @@ namespace Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+
             optionsBuilder.UseNpgsql(decryptStringConn());
-            encryptStringConn();    
+            System.IO.File.Delete(fileDecrypted);
         }
 
         public string decryptStringConn()
         {
-            File.Decrypt(connStringFile);
-            return System.IO.File.ReadAllText(connStringFile).ToString();
+            importKeys();
+            return decryptFile(connStringFile);
         }
 
-        public void encryptStringConn() { 
-            File.Encrypt(connStringFile);
+        public void importKeys() {
+            StreamReader sr = new StreamReader(keysFile);
+            string keytxt = sr.ReadToEnd();
+            rsa = new RSACryptoServiceProvider(cspp);
+            rsa.FromXmlString(keytxt);
+            rsa.PersistKeyInCsp = true;
+            if (rsa.PublicOnly == true)
+                Console.WriteLine("Key: " + cspp.KeyContainerName + " - Public Only");
+            else
+                Console.WriteLine("Key: " + cspp.KeyContainerName + " - Full Key Pair");
+            sr.Close();
         }
+
+        static string decryptFile(string inFile) 
+        {
+            // Create instance of Aes for
+            // symetric decryption of the data.
+            Aes aes = Aes.Create();
+
+            // Create byte arrays to get the length of
+            // the encrypted key and IV.
+            // These values were stored as 4 bytes each
+            // at the beginning of the encrypted package.
+            byte[] LenK = new byte[4];
+            byte[] LenIV = new byte[4];
+
+            // Construct the file name for the decrypted file.
+            string outFile = fileDecrypted;
+
+            // Use FileStream objects to read the encrypted
+            // file (inFs) and save the decrypted file (outFs).
+            using (FileStream inFs = new FileStream(inFile, FileMode.Open))
+            {
+
+                inFs.Seek(0, SeekOrigin.Begin);
+                inFs.Seek(0, SeekOrigin.Begin);
+                inFs.Read(LenK, 0, 3);
+                inFs.Seek(4, SeekOrigin.Begin);
+                inFs.Read(LenIV, 0, 3);
+
+                // Convert the lengths to integer values.
+                int lenK = BitConverter.ToInt32(LenK, 0);
+                int lenIV = BitConverter.ToInt32(LenIV, 0);
+
+                // Determine the start postition of
+                // the ciphter text (startC)
+                // and its length(lenC).
+                int startC = lenK + lenIV + 8;
+                int lenC = (int)inFs.Length - startC;
+
+                // Create the byte arrays for
+                // the encrypted Aes key,
+                // the IV, and the cipher text.
+                byte[] KeyEncrypted = new byte[lenK];
+                byte[] IV = new byte[lenIV];
+
+                // Extract the key and IV
+                // starting from index 8
+                // after the length values.
+                inFs.Seek(8, SeekOrigin.Begin);
+                inFs.Read(KeyEncrypted, 0, lenK);
+                inFs.Seek(8 + lenK, SeekOrigin.Begin);
+                inFs.Read(IV, 0, lenIV);
+
+                // Use RSACryptoServiceProvider
+                // to decrypt the AES key.
+                byte[] KeyDecrypted = rsa.Decrypt(KeyEncrypted, false);
+
+                // Decrypt the key.
+                ICryptoTransform transform = aes.CreateDecryptor(KeyDecrypted, IV);
+
+
+                // Decrypt the cipher text from
+                // from the FileSteam of the encrypted
+                // file (inFs) into the FileStream
+                // for the decrypted file (outFs).
+                using (FileStream outFs = new FileStream(outFile, FileMode.Create))
+                {
+
+                    int count = 0;
+                    int offset = 0;
+
+                    // blockSizeBytes can be any arbitrary size.
+                    int blockSizeBytes = aes.BlockSize / 8;
+                    byte[] data = new byte[blockSizeBytes];
+
+                    // By decrypting a chunk a time,
+                    // you can save memory and
+                    // accommodate large files.
+
+                    // Start at the beginning
+                    // of the cipher text.
+                    inFs.Seek(startC, SeekOrigin.Begin);
+                    using (CryptoStream outStreamDecrypted = new CryptoStream(outFs, transform, CryptoStreamMode.Write))
+                    {
+                        do
+                        {
+                            count = inFs.Read(data, 0, blockSizeBytes);
+                            offset += count;
+                            outStreamDecrypted.Write(data, 0, count);
+                        }
+                        while (count > 0);
+
+                        outStreamDecrypted.FlushFinalBlock();
+                        outStreamDecrypted.Close();
+                    }
+                    outFs.Close();
+                }   
+                inFs.Close();
+                return System.IO.File.ReadAllText(fileDecrypted);
+
+               }      
+        }
+
     }
 }
