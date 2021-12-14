4cf0fa40c98e4cf9fbba5753f7f4ca5ec115878c
oriol <obassols8@boscdelacoma.cat>
2021-12-14 16:44:39 +0100

    Login True/False

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -21,9 +21,17 @@ namespace Controllers
         [Route("Equips")]
         public async Task<List<Equip>> GetEquips()
         {
-            var myTask = Task.Run(() => context.Equips.ToList());
+            var myTask = Task.Run(() => context.Equip.ToList());
             List<Equip> equips = await myTask;
             return equips;
+        }
+        [HttpGet]
+        [Route("Jugador|{username}|{contrasenya}")]
+        public async Task<Boolean> LogIn(string username, string contrasenya)
+        {
+            var myTask = Task.Run(() => context.Jugadors.Where(j => j.nom_jugador.Equals(username) && j.contrasenya_jugador.Equals(contrasenya)).ToList());
+            List<Jugador> player = await myTask;
+            return (player.Count<Jugador>() != 0);
         }
     }
 }
