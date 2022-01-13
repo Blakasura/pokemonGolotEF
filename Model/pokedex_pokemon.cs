c4a658fa9aa3abdbc302933bbf7c6761631d2661
danip <danip@DESKTOP-SP7DLLE>
2022-01-13 17:30:39 +0100

    Canvis Pokedex

--- Model/pokedex_pokemon.cs
+++ Model/pokedex_pokemon.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data;
 using Model;
 
-namespace Model
+namespace pokemonGolot.Model
 {
     public class pokedex_pokemon
     {
         public int id_pokemon { get; set; }
         public string nom_pokemon { get; set; }
         public string rarity { get; set; }
-        public List<Tipus> tipus { get; set; }
+        public List<string> tipus { get; set; }
         public bool vist_per_jugador { get; set; }
         public int caramels { get; set; }
+
     }
 }
 
