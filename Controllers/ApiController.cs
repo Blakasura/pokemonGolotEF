0fa37b8e64ca55e8a567541c3bfc6721f3b67669
danip <danip@DESKTOP-SP7DLLE>
2022-03-17 16:37:53 +0100

    prova jugadors

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -20,8 +20,7 @@ namespace Controllers
             List<Jugador> jugadors = await myTask;
 
             foreach (Jugador jugador in jugadors) {
-                Console.WriteLine(Encryption.Decrypt("uNuUTparKDXQcSAnBylPNA=="));
-                //Console.WriteLine(Encryption.Decrypt(jugador.nom_jugador));
+                Console.WriteLine(jugador.nom_jugador.ToString());
                 //jugador.nom_jugador = Encryption.Decrypt(jugador.nom_jugador);
                 //jugador.email_jugador = Encryption.Decrypt(jugador.email_jugador);
             }
